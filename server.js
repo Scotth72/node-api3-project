807be21fc44b79b79b392e4fe34d86aacaf778a1
Heath Scott <HeathScott@Heaths-MacBook-Pro.local>
2020-05-07 03:09:04 -0500

    created custom logger

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 
 const server = express();
+server.use(logger);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+	res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+	console.log(`Log, ${req.method} to ${req.orginalUrl} on ${(uuid.v4(), msg)}`);
+	next();
+}
 
 module.exports = server;
